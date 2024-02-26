9c244331e301e276ab58ae5fe3874ac1a5ab6593
adrian1 <a.montesl.2020@alumnos.urjc.es>
2024-02-26 12:20:22 +0100

    InicioFInal

--- hmacsha1.c
+++ hmacsha1.c
@@ -2,54 +2,71 @@
 #include <openssl/err.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define SHA1_LEN 20
-#define BUFFER_SIZE 1024
+#include <string.h>
 
 void handleErrors(void) {
     ERR_print_errors_fp(stderr);
-    abort();
+    exit(1);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Uso: %s <archivo>\n", argv[0]);
-        return -1;
+    if (argc != 3) {
+        printf("Uso: %s <archivo> <clave>\n", argv[0]);
+        return 1;
     }
 
-    FILE *fp = fopen(argv[1], "rb"); // Abre en modo binario
-    if (fp == NULL) {
-        perror("No se puede abrir el fichero");
-        return -1;
+    FILE *fp = fopen(argv[1], "rb");
+    if (!fp) {
+        perror("No se puede abrir el archivo");
+        return 1;
     }
 
-    EVP_MD_CTX *c_digest = EVP_MD_CTX_new();
-    if (c_digest == NULL) handleErrors();
+    // La clave para el HMAC
+    unsigned char *key = (unsigned char *)argv[2];
+    size_t key_len = strlen(argv[2]);
 
-    if (1 != EVP_DigestInit_ex(c_digest, EVP_sha1(), NULL)) handleErrors();
+    // Inicializar variables para OpenSSL
+    EVP_MAC *hmac = EVP_MAC_fetch(NULL, "HMAC", NULL);
+    if (!hmac) handleErrors();
 
-    unsigned char buffer[BUFFER_SIZE];
+    EVP_MAC_CTX *hmac_ctx = EVP_MAC_CTX_new(hmac);
+    if (!hmac_ctx) handleErrors();
+
+    OSSL_PARAM params[] = {
+        OSSL_PARAM_construct_utf8_string("digest", "SHA1", 0),
+        OSSL_PARAM_construct_octet_string("key", key, key_len),
+        OSSL_PARAM_construct_end()
+    };
+
+    if (1 != EVP_MAC_CTX_set_params(hmac_ctx, params))
+        handleErrors();
+
+    if (1 != EVP_MAC_init(hmac_ctx, key, key_len, NULL)) // Corregido aquí
+        handleErrors();
+
+    // Leer el archivo y alimentar los datos al HMAC
+    unsigned char buffer[1024];
     size_t bytes_read;
-
-    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, fp)) > 0) {
-        if (1 != EVP_DigestUpdate(c_digest, buffer, bytes_read)) handleErrors();
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
+        if (1 != EVP_MAC_update(hmac_ctx, buffer, bytes_read))
+            handleErrors();
     }
-
     fclose(fp);
 
-    unsigned char hash[SHA1_LEN];
-    unsigned int sz = SHA1_LEN;
+    // Obtener el resultado del HMAC
+    unsigned char result[EVP_MAX_MD_SIZE];
+    size_t result_len;
+    if (1 != EVP_MAC_final(hmac_ctx, result, &result_len, sizeof(result)))
+        handleErrors();
 
-    if (1 != EVP_DigestFinal_ex(c_digest, hash, &sz)) handleErrors();
+    EVP_MAC_CTX_free(hmac_ctx);
+    EVP_MAC_free(hmac);
 
-    EVP_MD_CTX_free(c_digest);
-
-    for (unsigned int i = 0; i < sz; i++) {
-        printf("%02x", hash[i]);
+    // Imprimir el HMAC en hexadecimal
+    for (size_t i = 0; i < result_len; i++) {
+        printf("%02x", result[i]);
     }
     printf("\n");
 
     return 0;
 }
-
-
